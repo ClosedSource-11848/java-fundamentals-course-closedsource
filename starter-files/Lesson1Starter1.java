e2a4c000874028484f47d0557940866336fb8d46
Zock2005 <marcelomarv3.0@gmail.com>
2026-06-16 23:53:13 -0500

    feat: add starter templates for lesson 1 exercises.

---
+++ starter-files/Lesson1Starter1.java
@@ -0,0 +1,15 @@
+
+/*  Ejercicio: Escribir un mensaje    */
+/*  Objetivo: El usuario sabe como programar    */
+
+public class Start {
+    public static void main(String[] args) {
+
+/*====================================================================*/
+
+    /*  El texto es de su elección, mejor si haces varios  */
+
+/*====================================================================*/
+
+    }
+}
---
+++ starter-files/Lesson1Starter2.java
@@ -0,0 +1,21 @@
+
+/*  Ejercicio: Detectar errores en el código    */
+/*  Objetivo: Identificar errores de escritura  */
+
+public class Syntax {
+    public static void main(String[] args) {
+
+/*====================================================================*/
+
+        System.out.println("Sistema iniciado...");
+        System.out.println("Hola usuario");
+        System.out.println("Bienvenido al curso de programación)
+        System.out.println("Hoy aprenderás Java");
+        system.out.println("Recuerda practicar siempre");
+        System.out.println("Exitos :)
+        System.out.println("Fin del programa");
+
+/*====================================================================*/
+
+    }
+}
