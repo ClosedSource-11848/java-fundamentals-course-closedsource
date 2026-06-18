d64c492b1ed6f49680f0183817add95798566d64
V8Z5 <marlonvizaquispe@gmail.com>
2026-06-17 22:11:50 -0500

    feat: add lesson 3 examples

---
+++ completed-examples/Lesson3Example3.java
@@ -0,0 +1,17 @@
+public class MiProgramaWhile {
+    public static void main(String[] args) {
+        
+        // WHILE
+        int energia = 3;
+
+        System.out.println("Nivel de energia:");
+
+        while (energia > 0) {
+            System.out.println("Energia: " + energia);
+            energia--;
+        }
+
+        System.out.println("Sin energia");
+        
+    }
+}
