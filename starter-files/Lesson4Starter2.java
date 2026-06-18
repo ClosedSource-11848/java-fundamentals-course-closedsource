5e7b336f835de0fd81e6f8a86952ec0eba65f27b
Mauricio Castillo Yataco <111830574+M4uricioCastillo@users.noreply.github.com>
2026-06-17 20:16:11 -0500

    add starters from lesson 4

---
+++ starter-files/Lesson4Starter2.java
@@ -0,0 +1,21 @@
+// Importamos la herramienta para leer el teclado
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner teclado = new Scanner(System.in);
+        
+        System.out.println("¡Bienvenido! Ingresa tu nombre de jugador:");
+        // [ALUMNO]: 1. Declara una variable String llamada 'nombre' y usa teclado.nextLine() para leerla
+        
+        
+        System.out.println("¿Cuántas vidas quieres tener en esta partida?");
+        // [ALUMNO]: 2. Declara una variable int llamada 'vidas' y usa teclado.nextInt() para leerla
+        
+        
+        // [ALUMNO]: 3. Usa System.out.println para imprimir un mensaje uniendo el 'nombre' y las 'vidas'
+        // Ejemplo esperado: "¡A jugar, [nombre]! Tienes [vidas] vidas."
+        
+        
+    }
+}
